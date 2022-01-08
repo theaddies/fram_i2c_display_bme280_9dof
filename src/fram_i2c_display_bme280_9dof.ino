2975d860866a8c71a6fb53786b08dba9abdc228a
Andrew Addie <theaddies@neo.rr.com>
2022-01-08 10:45:37 -0500

    toptechboy added and working but 180 degrees out

--- src/fram_i2c_display_bme280_9dof.ino
+++ src/fram_i2c_display_bme280_9dof.ino
@@ -118,7 +118,26 @@ volatile unsigned long ContactBounceTime; // Timer to avoid contact bounce in in
 float WindSpeed; // speed miles per hour
 
 #define Offset 0;
-
+//**********************************
+//define values for toptechboy compass measurement
+float thetaM;
+float phiM;
+float thetaFold=0;
+float thetaFnew;
+float phiFold=0;
+float phiFnew;
+float thetaG=0;
+float phiG=0;
+float theta;
+float phi;
+float thetaRad;
+float phiRad;
+float Xm;
+float Ym;
+float psi;
+float dt;
+float bno_compass_heading;
+unsigned long millisOld;
 /**************************************************************************/
 /*
     Arduino setup function (automatically called at startup)
@@ -304,7 +323,7 @@ if (i2ceeprom.begin(0x50)) {  // you can stick the new i2c addr in here, e.g. be
 
    /* Crystal must be configured AFTER loading calibration data into BNO055. */
     bno.setExtCrystalUse(true);
-
+millisOld=millis();
     sensors_event_t event;
     bno.getEvent(&event);
     /* always recal the mag as It goes out of calibration very often */
@@ -511,6 +530,9 @@ printValues();
   Serial.print("compass heading:  ");
   Serial.print(compass_heading, 4);
 
+  bno_compass_heading = get_compass_heading();
+  Serial.print("psi from bno055\n");
+  Serial.print(bno_compass_heading);
 
 
       adafruit_bno055_offsets_t newCalib;
@@ -705,4 +727,80 @@ Rotations++;
 ContactBounceTime = millis();
 }
 
+}
+
+float get_compass_heading() {
+  // put your main code here, to run repeatedly:
+uint8_t system, gyro, accel, mg = 0;
+bno.getCalibration(&system, &gyro, &accel, &mg);
+imu::Vector<3> acc =bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+imu::Vector<3> gyr =bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+imu::Vector<3> mag =bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
+//measured value of tilt in x
+thetaM=-atan2(acc.x()/9.8,acc.z()/9.8)/2/3.141592654*360;
+//measured value of tilt in y
+phiM=-atan2(acc.y()/9.8,acc.z()/9.8)/2/3.141592654*360;
+//filtered value for tilt in y
+phiFnew=.95*phiFold+.05*phiM;
+//filtered value for tilt in x
+thetaFnew=.95*thetaFold+.05*thetaM;
+ //this is the time through one loop of program
+dt=(millis()-millisOld)/1000.;
+millisOld=millis();
+//measures tilt in x with filter to remove vibration but still quick to respond.  lesson 9
+//complimentary filter
+theta=(theta+gyr.y()*dt)*.95+thetaM*.05;
+//measures tilt in y with filter to remove vibration but still quick to respond.  lesson 9
+phi=(phi-gyr.x()*dt)*.95+ phiM*.05;
+
+thetaG=thetaG+gyr.y()*dt;
+phiG=phiG-gyr.x()*dt;
+ 
+phiRad=phi/360*(2*3.14);
+thetaRad=theta/360*(2*3.14);
+ 
+ //tilt compensated x
+Xm=mag.x()*cos(thetaRad)-mag.y()*sin(phiRad)*sin(thetaRad)+mag.z()*cos(phiRad)*sin(thetaRad);
+//tilt compensated y
+Ym=mag.y()*cos(phiRad)+mag.z()*sin(phiRad);
+ //compass heading
+psi=atan2(Ym,Xm)/(2*3.14)*360;
+ 
+Serial.print(acc.x()/9.8);
+Serial.print(",");
+Serial.print(acc.y()/9.8);
+Serial.print(",");
+Serial.print(acc.z()/9.8);
+Serial.print(",");
+Serial.print(accel);
+Serial.print(",");
+Serial.print(gyro);
+Serial.print(",");
+Serial.print(mg);
+Serial.print(",");
+Serial.print(system);
+Serial.print(",");
+Serial.print(thetaM);
+Serial.print(",");
+Serial.print(phiM);
+Serial.print(",");
+Serial.print(thetaFnew);
+Serial.print(",");
+Serial.print(phiFnew);
+Serial.print(",");
+Serial.print(thetaG);
+Serial.print(",");
+Serial.print(phiG);
+Serial.print(",");
+Serial.print(theta);
+Serial.print(",");
+Serial.print(phi);
+Serial.print(",");
+Serial.println(psi);
+ 
+phiFold=phiFnew;
+thetaFold=thetaFnew;
+ 
+ return(psi);
+//delay(BNO055_SAMPLERATE_DELAY_MS);
 }
